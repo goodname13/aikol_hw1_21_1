339f4c72c6b8b62fec663179952722fe6e806452
goodname13 <112273818+goodname13@users.noreply.github.com>
2022-08-29 14:16:55 +0300

    Add files via upload

---
+++ aikol_21_1_hw1_2mouse.py
@@ -0,0 +1,29 @@
+class Cow:
+    def init(self, name):
+        self.name = name
+        
+
+    def voice(self):
+        print(f'{self.name}:Muu')
+        class dog:
+               def init(self, name):
+                  self.name = name
+        
+
+    def voice(self):
+        print(f'{self.name}:gav')
+        class bear:
+            def init(self, name):
+               self.name = name
+        
+
+    def voice(self):
+        print(f'{self.name}:arrrr')
+        class cat:
+           def init(self, name):
+            self.name = name
+        
+
+    def voice(self):
+        print(f'{self.name}:myau')
+
